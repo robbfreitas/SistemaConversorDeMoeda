d779e47350570de2e6b65e523c755817ac523efa
robbfreitas <robertocompromissos@gmail.com>
2022-12-06 19:51:25 -0300

    Add project files.

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaConversorDeMoeda
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 0.06;
+        public static double Cotacao;
+        public static double Dolares;
+
+        public static double ValorFinal(double cotacao, double dolares)
+        {
+            return cotacao * dolares + (cotacao * dolares * IOF);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace SistemaConversorDeMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double calculoFinal = ConversorDeMoeda.ValorFinal(dolares, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais: " + calculoFinal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
